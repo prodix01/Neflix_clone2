15885812c0e69e02ee4a29843ce47f579256b7f0
kim <s158614@nishitech.ac.jp>
2019-12-27 12:02:08 +0900

    Api #1

--- README.md
+++ README.md
@@ -7,3 +7,13 @@
 - [ ] TV Shows
 - [ ] Search
 - [ ] Detail
+
+## API Verbs
+- [ ] Now Playing (Movie)
+- [ ] Upcoming (Movie)
+- [ ] Top Rated (Tv)
+- [ ] Popular (Tv, Movie)
+- [ ] Airing Today (Tv)
+- [ ] Tv Show Detail
+- [ ] Movie Detail
+- [ ] Search (Movie, Tv)
---
+++ src/api.js
@@ -0,0 +1,21 @@
+import axios from "axios";
+
+const api = axios.create({
+    baseURL: "https://api.themoviedb.org/3/",
+    params: {
+        api_key: "4e1aa96a568add36e8e3cbc4274fd907",
+        language : "ko-KR"
+    }
+});
+
+export const moviesApi = {
+    nowPlaying: () => api.get("movie/now_playing"),
+    upComing: () => api.get("movie/upcoming"),
+    popular: () => api.get("movie/popular")
+};
+
+export const tvApi = {
+    popular: () => api.get("tv/popular"),
+    topRate: () => api.get("tv/top_rated"),
+    airingToday: () => api.get("tv/airing_today")
+};
