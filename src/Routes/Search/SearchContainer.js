65ecd6633468c42a21f6cbbf179fdb4d65b285e2
kim <s158614@nishitech.ac.jp>
2019-12-29 20:28:51 +0900

    Add Search container& presenter #1

---
+++ src/Routes/Search/SearchContainer.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+import SearchPresenter from "./SearchPresenter";
+
+class SearchContainer extends Component {
+    render() {
+        return (
+            <div>
+                <h1>Search</h1>
+            </div>
+        );
+    }
+}
+
+export default SearchContainer;
---
+++ src/Routes/Search/SearchPresenter.js
@@ -0,0 +1,24 @@
+import React from "react";
+import PropTypes from "prop-types"
+
+const SearchPresenter = ({
+    movieResults,
+    tvResults,
+    error,
+    loading,
+    searchTerm,
+    handleSubmit
+}) => null;
+
+SearchPresenter.propTypes = {
+    movieResults : PropTypes.array,
+    tvResults: PropTypes.array,
+    error: PropTypes.string,
+    loading: PropTypes.bool.isRequired,
+    searchTerm: PropTypes.string,
+    handleSubmit: PropTypes.func.isRequired
+};
+
+
+
+export default SearchPresenter;
