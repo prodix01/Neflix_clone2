ba583eb7f0cac7e322ba43108123f1b561ad2303
kim <s158614@nishitech.ac.jp>
2019-12-29 20:45:12 +0900

    Add SearchTerm funtion

--- src/Routes/Search/SearchContainer.js
+++ src/Routes/Search/SearchContainer.js
@@ -1,6 +1,6 @@
 import React, {Component} from 'react';
 import SearchPresenter from "./SearchPresenter";
-
+import {tvApi, moviesApi} from "../../api";
 class SearchContainer extends Component {
 
     state = {
@@ -12,8 +12,38 @@ class SearchContainer extends Component {
     };
 
     handleSubmit = () => {
+        const {searchTerm} = this.state;
+        if (searchTerm !== null) {
+            // 검색실행
+            this.searchByTerm();
 
+        }
     };
+
+    //search by Term
+    searchByTerm = async () => {
+
+        const {searchTerm} = this.state;
+
+        this.setState({loading: true});
+
+        try {
+            const {
+                data: { results: movieResults}
+            } = await moviesApi.search(searchTerm);
+            const {
+                data: {results: tvResults}
+            } = await tvApi.search(searchTerm);
+            this.setState({tvResults, movieResults})
+        }
+        catch {
+            this.setState({error: "No Search Results"});
+        }
+        finally {
+            this.setState({loading: false});
+        }
+    };
+
 
 
     render() {
@@ -24,6 +54,10 @@ class SearchContainer extends Component {
             loading,
             searchTerm
         } = this.state;
+
+
+        console.log(movieResults);
+
         return (
             <SearchPresenter
                 loading={loading}
